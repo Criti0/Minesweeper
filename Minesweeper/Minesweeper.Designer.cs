a9660c4ec96c09de51aedbda07020906a0d03f75
Gheorghe-Cristian Manasia <134348863+Criti0@users.noreply.github.com>
2024-03-03 06:45:07 +0200

    Add project files.

---
+++ Minesweeper/Minesweeper.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Minesweeper
+{
+    partial class Minesweeper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Minesweeper));
+            this.tiles = new System.Windows.Forms.Panel();
+            this.Punctaj_label = new System.Windows.Forms.Label();
+            this.Scor_label = new System.Windows.Forms.Label();
+            this.img = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // tiles
+            // 
+            this.tiles.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.tiles.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tiles.Location = new System.Drawing.Point(23, 12);
+            this.tiles.Name = "tiles";
+            this.tiles.Size = new System.Drawing.Size(535, 494);
+            this.tiles.TabIndex = 0;
+            this.tiles.Paint += new System.Windows.Forms.PaintEventHandler(this.tiles_Paint);
+            // 
+            // Punctaj_label
+            // 
+            this.Punctaj_label.AutoEllipsis = true;
+            this.Punctaj_label.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Punctaj_label.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Punctaj_label.Location = new System.Drawing.Point(488, 550);
+            this.Punctaj_label.Name = "Punctaj_label";
+            this.Punctaj_label.Size = new System.Drawing.Size(70, 28);
+            this.Punctaj_label.TabIndex = 1;
+            this.Punctaj_label.Text = "0";
+            // 
+            // Scor_label
+            // 
+            this.Scor_label.AutoSize = true;
+            this.Scor_label.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Scor_label.Location = new System.Drawing.Point(410, 551);
+            this.Scor_label.Name = "Scor_label";
+            this.Scor_label.Size = new System.Drawing.Size(72, 27);
+            this.Scor_label.TabIndex = 2;
+            this.Scor_label.Text = "Scor:";
+            this.Scor_label.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // img
+            // 
+            this.img.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("img.ImageStream")));
+            this.img.TransparentColor = System.Drawing.Color.Transparent;
+            this.img.Images.SetKeyName(0, "bomb.png");
+            this.img.Images.SetKeyName(1, "coin.png");
+            this.img.Images.SetKeyName(2, "tile1.png");
+            this.img.Images.SetKeyName(3, "tile2.png");
+            // 
+            // Minesweeper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(582, 583);
+            this.Controls.Add(this.Scor_label);
+            this.Controls.Add(this.Punctaj_label);
+            this.Controls.Add(this.tiles);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Minesweeper";
+            this.Text = "Minesweeper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel tiles;
+        private System.Windows.Forms.Label Punctaj_label;
+        private System.Windows.Forms.Label Scor_label;
+        private System.Windows.Forms.ImageList img;
+    }
+}
+
---
+++ Minesweeper/Minesweeper.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Minesweeper : Form
+    {
+
+        int Score = 0;
+        PictureBox[] tile_block = new PictureBox[100];
+        public Minesweeper()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            load_tiles();
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void tiles_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        private void load_tiles()
+        {
+            
+
+            int x = 0;
+            int y = 0;
+            int k = 0;
+            int t=0;
+            int tileControl = 0;
+
+            Random rand = new Random();
+            for(int i=0;i<100;i++)
+            {
+                int ran=rand.Next(0,2);
+
+                tile_block[i]= new PictureBox();
+                tile_block[i].Height = 40;
+                tile_block[i].Width = 40;
+                tile_block[i].Left = x;
+                tile_block[i].Top = y;
+                tile_block[i].BorderStyle = BorderStyle.FixedSingle;
+                
+
+                tile_block[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                tile_block[i].Click += new System.EventHandler(ClickTile);
+
+                //tile[i].Image = img.Images[ran];
+                if(ran==0)
+                {
+                    tile_block[i].Tag = "Bomb";
+                }
+                else
+                {
+                    tile_block[i].Tag = "Money";
+                }
+
+                tiles.Controls.Add(tile_block[i]);
+                x = x + 40;
+                k++;
+
+                if((t+tileControl)%2==0)
+                {
+                    tile_block[i].Image = img.Images[2];
+                    t++;
+                }
+                else 
+                {
+                    tile_block[i].Image = img.Images[3];
+                    t++;
+                }
+                
+               
+                
+
+                if(k==10)
+                {
+                    x = 0;
+                    y = y + 40;
+                    k = 0;
+                    tileControl++;
+                }
+            }
+        }
+
+        private void ClickTile(Object sender, System.EventArgs e)
+        {
+            DialogResult ans;
+            PictureBox tile = (PictureBox)sender;
+            if(tile.Tag.ToString() !="Clicked")
+            {
+                if (tile.Tag.ToString() == "Bomb")
+                {
+                    tile.Image = img.Images[0];
+                    ans = MessageBox.Show("Game Over :( , Your score: " + Score.ToString() + "\nPlay Again?", "Game Over", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (ans == DialogResult.Yes)
+                    {
+                        Score = 0;
+                        Punctaj_label.Text = "0";
+                        foreach (PictureBox pictureBox in tile_block)
+                        {
+                            tiles.Controls.Remove(pictureBox);
+                        }
+                        load_tiles();
+
+                    }
+                    else
+                    {   
+                        Application.Exit();
+                    }
+                }
+                else if (tile.Tag.ToString() == "Money")
+                {
+                    tile.Image = img.Images[1];
+                    tile.Tag = "Clicked";
+                    Score += 5;
+                    Punctaj_label.Text = Score.ToString();
+
+                }
+            }
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
